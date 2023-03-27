e3474c55d0eb988e2ffeeabdf3fbb640c52f576b
Natalie Tepedino <ntepedi0@email.cpcc.edu>
2023-03-26 22:59:53 -0700

    Revert "added print statement"
    This reverts commit b70560a18443bf0069bcb50710a775f6d913f908.

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,5 @@ public class helloWorld
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
    }
 }
